78241971c7f2825d212935be8b259ccb55b92407
Saurabh Pandey <118668564+Saurabh12Pandey@users.noreply.github.com>
2024-08-11 07:17:25 -0700

    Update Navbar.jsx
    Nav commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Hello  Shahrukh</h1>
+      <h1>Hello Saurabh</h1>
       <div className="wrapper">
         <div className="search">
           <input type="text" placeholder="Search..." />
